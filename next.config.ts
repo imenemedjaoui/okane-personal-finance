69549f1bc195352f5ca81a901c2381339acb6b6d
imenemedjaoui <mimi15mimo@gmail.com>
2026-04-06 20:59:12 +0100

    fix: next js config update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
   allowedDevOrigins: ['192.168.1.65'],
-}
-
+};
 
 export default nextConfig;
